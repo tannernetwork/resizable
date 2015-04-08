363c30bb44ada1b8673f1705e64925cf9854ad55
Tanner <tanner963@gmail.com>
2015-04-08 18:24:15 +0200

    repo

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # resizable
+
+jQuery plugin for resizable elements.
+
+## Install
+
+```
+npm install jquery-resizable
+```
+
+Then include `resizable.js` and `resizable.css` in your webapp.
+
+## Usage
+
+```js
+$('#some div').resizable( options )
+```
+
+It also applies for multiple elements.
+
+## Options
+
+#### `direction`
+
+Possible values for an array:
+* `top`
+* `right`
+* `bottom`
+* `left`
+
+As a string:
+* `horizontal` = `left` + `right`
+* `vertical` = `top` + `bottom`
+* `top`
+* `right`
+* `bottom`
+* `left`
+
+**Example**:
+```js
+$('#div1').resizable({
+    direction: ['bottom', 'left']
+})
+$('#div2').resizable({
+    direction: 'vertical'
+})
+```
+
+By default each direction is turned on.
---
+++ resizable.js
@@ -0,0 +1,166 @@
+/*!
+ * resizable 1.0.0
+ * https://github.com/tannernetwork/resizable
+ *
+ * Copyright 2015 Tanner (http://tanner.zone)
+ * Released under the MIT license
+ */
+
+(function ($) {
+ 
+    $.fn.resizable = function(options) {
+ 
+        var settings = $.extend({
+            direction: ['top', 'right', 'bottom', 'left']
+        }, options);
+
+        return this.each(function() {
+
+            var element = $(this);
+
+            var isDragging = false;
+            var initial = {};
+            var prepareHandles = {
+                top: false,
+                right: false,
+                bottom: false,
+                left: false
+            };
+            var handles = {};
+
+            element.addClass('resizable');
+
+            if(settings.direction instanceof Array)
+            {
+                for (var i = settings.direction.length - 1; i >= 0; i--) {
+                    switch(settings.direction[i])
+                    {
+                        case 'top':
+                        case 't':
+                        prepareHandles.top = true;
+                        break;
+                        case 'right':
+                        case 'r':
+                        prepareHandles.right = true;
+                        break;
+                        case 'bottom':
+                        case 'b':
+                        prepareHandles.bottom = true;
+                        break;
+                        case 'left':
+                        case 'l':
+                        prepareHandles.left = true;
+                        break;
+                    }
+                };
+            }
+            else if(typeof settings.direction == 'string')
+            {
+                switch(settings.direction)
+                {
+                    case 'vertical':
+                    case 'v':
+                    prepareHandles.top = true;
+                    prepareHandles.bottom = true;
+                    break;
+                    case 'horizontal':
+                    case 'h':
+                    prepareHandles.right = true;
+                    prepareHandles.left = true;
+                    case 'top':
+                    case 't':
+                    prepareHandles.top = true;
+                    break;
+                    case 'right':
+                    case 'r':
+                    prepareHandles.right = true;
+                    break;
+                    case 'bottom':
+                    case 'b':
+                    prepareHandles.bottom = true;
+                    break;
+                    case 'left':
+                    case 'l':
+                    prepareHandles.left = true;
+                    break;
+                }
+            }
+
+            if(prepareHandles.top)
+            {
+                handles.top = $('<div />').addClass('resizable-handle resizable-t').appendTo(element);
+            }
+            if(prepareHandles.right)
+            {
+                handles.right = $('<div />').addClass('resizable-handle resizable-r').appendTo(element);
+            }
+            if(prepareHandles.bottom)
+            {
+                handles.bottom = $('<div />').addClass('resizable-handle resizable-b').appendTo(element);
+            }
+            if(prepareHandles.left)
+            {
+                handles.left = $('<div />').addClass('resizable-handle resizable-l').appendTo(element);
+            }
+            
+            $(this).children('.resizable-l, .resizable-r, .resizable-t, .resizable-b').mousedown(function(e) {
+                var dir;
+                switch(true)
+                {
+                    case $(this).hasClass('resizable-l'):
+                    dir = 'l';
+                    break;
+                    case $(this).hasClass('resizable-r'):
+                    dir = 'r';
+                    break;
+                    case $(this).hasClass('resizable-t'):
+                    dir = 't';
+                    break;
+                    case $(this).hasClass('resizable-b'):
+                    dir = 'b';
+                    break;
+                }
+                isDragging = true;
+                initial = {
+                    x: e.clientX,
+                    y: e.clientY,
+                    height: element.height(),
+                    width: element.width(),
+                    direction: dir
+                };
+
+                $('html').addClass('resizable-resizing resizable-resizing-'+initial.direction);
+            });
+
+            $(window).mousemove(function(e) {
+                if(isDragging)
+                {
+                    var moveX = e.clientX-initial.x;
+                    var moveY = e.clientY-initial.y;
+
+                    switch(initial.direction)
+                    {
+                        case 'r':
+                        element.width(initial.width+moveX);
+                        break;
+                        case 'l':
+                        element.width(initial.width-moveX);
+                        break;
+                        case 'b':
+                        element.height(initial.height+moveY);
+                        break;
+                        case 't':
+                        element.height(initial.height-moveY);
+                        break;
+                    }
+                }
+            }).mouseup(function(e) {
+                isDragging = false;
+                $('html').removeClass('resizable-resizing resizable-resizing-'+initial.direction);
+            });
+
+        });
+
+    }
+ 
+}(jQuery));
